0ea377b329133e6bfcc8d64bd1b84f689aa0a838
Lucas Caramelo <lcaram@br.ibm.com>
2017-12-06 20:09:58 -0200

    refactor

--- app/src/main/java/br/com/fiap/placarapp/Extras.java
+++ app/src/main/java/br/com/fiap/placarapp/Extras.java
@@ -4,7 +4,7 @@ package br.com.fiap.placarapp;
  * Created by lucascaramelo on 04/12/17.
  */
 
-public final class Common {
+public final class Extras {
     public static final String EXTRA_TIME_CASA = "extra_time_casa";
     public static final String EXTRA_TIME_VISITANTE = "extra_time_visitante";
 }
